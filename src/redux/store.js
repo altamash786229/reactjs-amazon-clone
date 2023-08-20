74a6c0a528cab09454d2397e6e7c0cd66dc347ce
altamash786229 <142254684+altamash786229@users.noreply.github.com>
2023-08-20 08:56:45 +0000

    Updates

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { cartReducer } from "./cartReducer";
-import { userReducer } from "./userReducer";
-import { createStore, combineReducers } from "redux";
-
-const store = createStore(combineReducers({ cartReducer, userReducer }));
-
-export default store;
